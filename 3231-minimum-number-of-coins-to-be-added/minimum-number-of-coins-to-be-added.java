90f57c43b5ec317d799ea61ae019ea5a93aa9ea3
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-08-07 00:04:16 +0530

    Time: 22 ms (88.96%) | Memory: 62.1 MB (24.73%) - LeetSync

--- 3231-minimum-number-of-coins-to-be-added/minimum-number-of-coins-to-be-added.java
+++ 3231-minimum-number-of-coins-to-be-added/minimum-number-of-coins-to-be-added.java
@@ -6,10 +6,15 @@ class Solution {
         int cnt=0;
         int i=0,j=0;
 
+        // here sum signifies the range of the target covered i.e. [1,sum] i.e the smallest number to formed
+        // logic is to start from 1, if the sum is less than the current element of the coins
+        // then sum itself has to taken in the set, and ergo range expands to [1 , 2*sum-1]
         while( sum <= target){
             if( i < coins.length && sum >= coins[i]){
                 sum += coins[i++] ; 
             }else{
+                // this signifies that new taken number is sum itself, thus range is expanded from
+                // [1 ... 2*sum-1]
                 sum += sum; 
                 cnt++;
             }
