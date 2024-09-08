4f63cb7b127f634eebcc9ea249ce06f28aa5d053
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-09-08 22:26:17 +0530

    Time: 103 ms (69.18%) | Memory: 100.2 MB (47.03%) - LeetSync

---
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
@@ -0,0 +1,40 @@
+class Pair{
+    int val1;
+    int val2;
+    Pair(int v1,int v2){
+        val1  = v1;
+        val2 = v2;
+    }
+}
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        Arrays.sort(intervals,(a,b)-> a[0]!=b[0]?a[0]-b[0]:a[1]-b[1]);
+        
+        Pair[] mQ = new Pair[queries.length];
+        for(int i=0;i<queries.length;i++){
+            mQ[i] = new Pair(queries[i],i);
+        }
+        Arrays.sort(mQ,(a,b)-> a.val1 != b.val1 ? a.val1 - b.val1:a.val2-b.val2);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.val2 != b.val2 ?a.val2-b.val2:a.val1-b.val1);
+        int ans[] = new int[queries.length];
+        int k =0;
+        for(int i=0;i<queries.length;i++){
+            int t = mQ[i].val1;
+            int indx = mQ[i].val2;
+            while(k<intervals.length && intervals[k][0] <= t){
+                pq.add(new Pair(intervals[k][0],intervals[k][1]-intervals[k][0]+1));
+                k++;
+            }
+            while(!pq.isEmpty() && pq.peek().val1+pq.peek().val2-1 < t)
+                pq.poll();
+            if(pq.isEmpty()){
+                ans[indx] = -1;
+            }else{
+                ans[indx] = pq.peek().val2;
+            }
+
+        }
+        return ans;
+
+    }
+}
