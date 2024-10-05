aed81e98068d5273476172682872a86406187db6
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-05 12:16:17 +0530

    Time: 234 ms (13.62%) | Memory: 45.7 MB (6.52%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length())
+            return false;
+        int freqS1[] = new int[26];
+        int freqS2[] = new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            freqS1[s1.charAt(i) - 'a']++;
+            freqS2[s2.charAt(i) - 'a']++;
+        }
+        int count=0;
+        for(int i=0;i<freqS1.length;i++){
+            if(freqS1[i] == freqS2[i]){
+                count++;
+            }
+        }
+
+        for(int i=0;i<s2.length()-s1.length();i++){
+            System.out.println(Arrays.toString(freqS1) + "\n"+ Arrays.toString(freqS2) +"\n" + count +"\n"); 
+            if(count == 26)
+                return true;
+
+            freqS2[s2.charAt(i+s1.length()) - 'a']++;
+            
+
+            if(freqS1[s2.charAt(i+s1.length()) - 'a'] == freqS2[s2.charAt(i+s1.length()) - 'a'])
+                count++;
+            else if(freqS1[s2.charAt(i+s1.length()) - 'a'] + 1 == freqS2[s2.charAt(i+s1.length()) - 'a'])
+                count--;
+
+            freqS2[s2.charAt(i) - 'a']--;
+
+            if(freqS1[s2.charAt(i) - 'a']  - 1 == freqS2[s2.charAt(i) - 'a'])
+                count--;
+            else if(freqS1[s2.charAt(i) - 'a']  == freqS2[s2.charAt(i) - 'a'])
+                count++;
+
+        }
+        if(count == 26)
+            return true;
+        return false;
+    }
+}
