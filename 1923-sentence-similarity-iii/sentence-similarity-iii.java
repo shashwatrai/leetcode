1069a74b6a25b3978f1ba122829bc86d725a2068
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-06 23:48:27 +0530

    Time: 1 ms (96.43%) | Memory: 41.7 MB (55.16%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -1,28 +1,22 @@
 class Solution {
     public boolean helper(String []list1,String []list2){
         
-        for(int i = 0;i<=list1.length;i++){
-            boolean front = true;
-            for(int j= 0;j<i;j++){
-                if(!list1[j].equals(list2[j])){
-                    front = false;
-                    break;
-                }
+        
+        // boolean front = true;
+        int indx = 0;
+        for(;indx<list1.length;indx++){
+            if(!list1[indx].equals(list2[indx])){
+                break;
             }
-            if(!front)
-                continue;
-            boolean  back = true;
-            int shift = list2.length - list1.length ; 
-            for(int j= i;j<list1.length;j++){
-                if(!list1[j].equals(list2[shift + j])){
-                    back = false;
-                    break;
-                }
-            }
-            if(back)
-                return true;
         }
-        return false;
+
+        int shift = list2.length - list1.length ; 
+        for(;indx<list1.length;indx++){
+            if(!list1[indx].equals(list2[shift + indx])){
+                break;
+            }
+        }        
+        return indx == list1.length;
     }
     public boolean areSentencesSimilar(String sentence1, String sentence2) {
 
