9f6619229366e2c551880f7495f89862717f2aa6
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-09 20:31:54 +0530

    Time: 1 ms (52.87%) | Memory: 44.4 MB (56.19%) - LeetSync

---
+++ 772-construct-quad-tree/construct-quad-tree.java
@@ -0,0 +1,72 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    
+
+    public Node helper(int [][]grid,int r1,int c1, int r2, int c2){
+
+        if(r1 == r2){
+            return new Node(grid[r1][c1] == 1 ? true:false,true);
+        }
+
+        
+        Node topLeft = helper(grid,r1,c1,r1 + (r2-r1+1)/2 -1,c1 + (c2-c1+1)/2 -1);
+        Node topRight = helper(grid,r1,c1 + (c2-c1+1)/2 ,r1 + (r2-r1+1)/2 - 1 ,c2);
+        Node bottomLeft = helper(grid,r1 + (r2-r1+1)/2,c1,r2,c1 + (c2-c1+1)/2 -1);
+        Node bottomRight = helper(grid,r1 + (r2-r1+1)/2 ,c1 + (c2-c1+1)/2 ,r2,c2);
+    
+        if( topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf 
+         && !( (topLeft.val ^ topRight.val) || (topLeft.val ^ bottomLeft.val) || (topLeft.val ^ bottomRight.val) )){
+            return new Node(topLeft.val , true);
+         }else{
+            Node curr = new Node();
+            curr.topLeft =topLeft;
+            curr.topRight = topRight;
+            curr.bottomLeft = bottomLeft;
+            curr.bottomRight = bottomRight;
+            return curr;
+         }
+        
+    }
+    public Node construct(int[][] grid) {
+        return helper(grid,0,0,grid.length-1,grid.length-1);
+    }
+}
