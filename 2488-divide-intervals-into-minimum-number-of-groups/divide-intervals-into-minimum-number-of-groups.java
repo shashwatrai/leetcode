1efdd644a6c9e9f1a80e87d30caa4872dd100a20
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-14 19:47:50 +0530

    Time: 295 ms (5.01%) | Memory: 108.4 MB (5.01%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -1,26 +1,17 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        int n = intervals.length;
-
-        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
-
         TreeMap<Integer,Integer> map = new TreeMap<>();
 
-        for(int i=0;i<n;i++){
-            Integer curr = map.floorKey(intervals[i][0]-1);
-
-            if(curr != null){
-               map.put(curr,map.get(curr)-1);
-               if(map.get(curr) == 0)
-                    map.remove(curr);
-            }
-            map.put(intervals[i][1],map.getOrDefault(intervals[i][1],0)+1);
-            // System.out.println(map);
+        for(int i=0;i<intervals.length;i++){
+            map.put(intervals[i][0],map.getOrDefault(intervals[i][0],0)+1);
+            map.put(intervals[i][1]+1,map.getOrDefault(intervals[i][1]+1,0)-1);
         }
-        int ans=0;
+        int max =0;
+        int curr=0;
         for(int i:map.keySet()){
-            ans += map.get(i);
+            curr += map.get(i);
+            max = Math.max(curr,max);
         }
-        return ans;
+        return max;
     }
 }
