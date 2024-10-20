82692bec074d585ea48a458e84fac8b20dbe013e
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-20 12:43:29 +0530

    Time: 10 ms (38.77%) | Memory: 44.3 MB (25.12%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,26 +1,32 @@
 class Solution {
-    int count;
-    public void helper(int []nums,int indx,int curr,int max){
-        
-        
+
+    public void helper(int []nums,int indx,Map<Integer,Integer> map){
 
         if(indx == nums.length){
-            if(curr == max){
-                count++;
-            }
             return;
         }
-        helper(nums,indx+1,curr,max);
-        helper(nums,indx+1,curr|nums[indx],max);
+        helper(nums,indx+1,map);
+
+        Map<Integer,Integer> map2 = new HashMap<>();
+        map2.put(nums[indx],1);
+        for(int i:map.keySet()){
+
+            map2.put( nums[indx]|i,map2.getOrDefault( nums[indx]|i,0) + map.get(i));
+        }
+        map2.forEach((key,value) -> {
+            map.merge(key,value, (v1,v2) -> v1+v2);
+        });
+        return ;
     }
 
     public int countMaxOrSubsets(int[] nums) {
-        count=0;
+        Map<Integer,Integer> map = new HashMap<>();
+       
         int max = 0;
         for(int i:nums){
             max |= i;
         }
-        helper(nums,0,0,max);
-        return count;
+        helper(nums,0,map);
+        return map.getOrDefault(max,0);
     }
 }
