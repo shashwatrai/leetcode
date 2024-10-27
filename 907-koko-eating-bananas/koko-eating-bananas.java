520c9828c368340824a3500aa03c60ab8ca82bba
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-27 08:19:56 +0530

    Time: 25 ms (46.94%) | Memory: 45.1 MB (36.08%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,25 +1,26 @@
 class Solution {
-    public boolean isPossible(int []piles,int speed,int h){
-        int t=0;
+    public boolean isValid(int []piles,int m, int h){
+        int cnt =0;
         for(int i:piles){
-            t += Math.ceil((double)i/speed);
+            cnt += Math.ceil((double)i/m);
         }
-        return t <= h;
+        return cnt <= h;
     }
     public int minEatingSpeed(int[] piles, int h) {
-        
-        int l = 1,r = 0;
-        for(int i:piles)
+        int r = piles[0];
+        for(int i:piles){
             r = Math.max(i,r);
-        
+        }
+        int l = 1;
+
         while(l<=r){
             int m = (l+r)/2;
-            if(isPossible(piles,m,h)){
+
+            if(isValid(piles,m,h))
                 r = m-1;
-            }else
+            else
                 l = m+1;
         }
-
         return l;
     }
 }
