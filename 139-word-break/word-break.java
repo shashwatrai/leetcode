1021f1e38d297ee5f99ac02e7b74349ceed09695
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-10-30 12:15:55 +0530

    Time: 3 ms (83.00%) | Memory: 41.5 MB (95.94%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,24 +1,22 @@
 class Solution {
-    Map<String,Boolean> map;
-    public boolean solve(String s, List<String> wordDict) {
-        if(s.length() == 0)
-            return true;
-        if(map.containsKey(s))
-            return map.get(s);
-        for(String i:wordDict){
-            if(i.length() <= s.length() && s.startsWith(i) && solve(s.substring(i.length(),s.length()),wordDict)){
-                map.put(s,true);
-                return true;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+
+        boolean []dp = new boolean[n];
+
+        for(int i=0;i<n;i++){
+
+            for(String word: wordDict){
+                
+                if(i - word.length()+1 < 0)
+                    continue;
+                
+                if((i-word.length() < 0 || dp[i-word.length()]) && s.startsWith(word,i-word.length()+1)){
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        map.put(s,false);
-        return false;
-    }
-    public boolean wordBreak(String s, List<String> wordDict) {
-        if(s.length() == 0)
-            return true;
-        map = new HashMap<>();
-        solve(s,wordDict);
-        return map.get(s);
+        return dp[n-1];
     }
 }
