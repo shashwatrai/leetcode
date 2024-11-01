d40b13b0f983ea707f8188006e62003cbe6d7db3
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2024-11-01 12:54:21 +0530

    Time: 2 ms (75.55%) | Memory: 41.1 MB (95.92%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -4,39 +4,26 @@ class Solution {
 
         for(char c: s.toCharArray()){
             freq[c-'a']++;
+            if(freq[c-'a'] > (1+s.length())/2)
+                return "";
         }
+        Integer indices[] = new Integer[26];
+        for(int i=0;i<26;i++)
+            indices[i] = i;
+        
+        Arrays.sort(indices,(a,b)-> freq[b] - freq[a]);
+        char ans[] =  new char[s.length()];
 
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b)-> freq[b - 'a']-freq[a-'a']);
-
+        int indx = 0;
         for(int i=0;i<26;i++){
-            if(freq[i] == 0)
-                continue;
-            pq.add((char)('a'+i));
-        }
-        StringBuilder stb = new StringBuilder();
-        char last = '.';
-        while(!pq.isEmpty()){
-            if(last == pq.peek()){
-                char charToBeSkipped = pq.poll();
-                if(pq.isEmpty()){
-                    return "";
+            while(freq[indices[i]]-- > 0){
+                ans[indx] = (char)('a'+indices[i]);
+                indx += 2;
+                if(indx >= s.length()){
+                    indx  = 1;
                 }
-                char curr = pq.poll();
-                freq[curr - 'a']--;
-                if(freq[curr - 'a'] != 0)
-                    pq.add(curr);
-                stb.append(curr);
-                last =curr;
-                pq.add(charToBeSkipped);
-            }else{
-                char curr = pq.poll();
-                freq[curr - 'a']--;
-                if(freq[curr - 'a'] != 0)
-                    pq.add(curr);
-                stb.append(curr);
-                last =curr;
             }
         }
-        return stb.toString();
+        return new String(ans);
     }
 }
