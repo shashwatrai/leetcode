85b3424f6442bf91a23cda7c310946ce5bd4c56e
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-01-04 12:58:35 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (91.05%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,46 +1,32 @@
 class Solution {
-    // public int find(int []arr,int target){
-    //     int l =0 ,r = arr.length-1;
-
-    //     while(l<=r){
-    //         int m = (l+r)/2;
-
-    //         if(arr[m] >= target)
-    //             r = m-1;
-    //         else
-    //             l = m+1;
-    //     }
-    //     return l;
-    // }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if(nums1.length > nums2.length)
             return findMedianSortedArrays(nums2,nums1);
 
+        int l =0,r= nums1.length;
+        int mid = (nums1.length + nums2.length + 1) /2;
 
-        int l=0,r = nums1.length;
-        int median  = (nums1.length+nums2.length+1)/2;
         while(l<=r){
-            int m = (l+r) /2;
-            int indx = median - m;
-           
-            int l1 = m > 0 ? nums1[m-1]:Integer.MIN_VALUE;
-            int r1 = m < nums1.length  ? nums1[m]:Integer.MAX_VALUE;
-            int l2 = indx > 0? nums2[indx-1]:Integer.MIN_VALUE;
-            int r2 = indx < nums2.length?nums2[indx]:Integer.MAX_VALUE;
-            // System.out.println(m+" "+indx+" "+l1+" "+r1+" "+l2+" "+r2);
+            int m = (l+r)/2;
+
+            int l1 = m > 0 ? nums1[m-1] : Integer.MIN_VALUE;
+            int r1 = m  < nums1.length ? nums1[m] : Integer.MAX_VALUE;
+
+            int l2 = mid - m  >  0 ? nums2[mid - m - 1]:Integer.MIN_VALUE;
+            
+            int r2 = mid - m < nums2.length ? nums2[mid - m] : Integer.MAX_VALUE;
+
             if(l1 <= r2 && l2 <= r1){
-                if((nums1.length+nums2.length)%2 == 0)
+                if((nums1.length + nums2.length)%2 == 0){
                     return (double)(Math.max(l1,l2) + Math.min(r1,r2))/2;
-                else 
+                }else{
                     return Math.max(l1,l2);
-            }else if(l2 > r1){
+                }
+            }else if(l1 > r2){
+                r = m-1;
+            }else
                 l = m+1;
-            } else{
-                r = m-1;  
-            }
-                
         }
-        
         return -1;
     }
 }
