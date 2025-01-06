05af56a008f8c8cfd00cdd164013410852d0e902
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-01-06 09:55:45 +0530

    Time: 4 ms (76.56%) | Memory: 44.9 MB (46.88%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,39 +1,27 @@
 class Solution {
-    int findIndx(List<Integer> arr, int t){
-        int l = 0,r = arr.size()-1;
-
-        while(l<=r){
-            int m = (l+r)/2;
-
-            if(arr.get(m) > t)
-                r = m-1;
-            else
-                l = m+1;
-        }
-        return r;
-    }
-
     public int[] minOperations(String boxes) {
-        List<Integer> setBits = new ArrayList<>();
-        List<Integer> prefixSum = new ArrayList<>();      
-        int n = boxes.length();
-
-        for(int i=0;i<n;i++){
+        int left = 0,leftCount=0;
+        int right=0,rightCount=0;
+        for(int i=0;i<boxes.length() ;i++)
             if(boxes.charAt(i) == '1'){
-                setBits.add(i);
-                prefixSum.add(i + (prefixSum.isEmpty()?0:prefixSum.get(prefixSum.size()-1)));
+                right+=i;
+                rightCount++;
+            }
+
+        int []ans = new int[boxes.length()];
+
+        for(int i=0;i<ans.length;i++){
+            ans[i] = (leftCount > 0 ? i*leftCount - left : 0) +
+                    (rightCount > 0 ? right - i*rightCount : 0);
+            
+            if(boxes.charAt(i) == '1'){
+                leftCount++;
+                left +=i;
+                rightCount--;
+                right-=i;
             }
         }
-
-        int []ans = new int[n];
-        if(setBits.size() == 0)
-            return ans;
-        for(int i=0;i<n;i++){
-            int indx = findIndx(setBits,i-1);
-
-            ans[i] = (indx >= 0 ? (indx+1)*i - prefixSum.get(indx):0) + 
-                    prefixSum.get(prefixSum.size()-1) - (indx >=0 ? prefixSum.get(indx) : 0) - i * (prefixSum.size() - indx - 1);
-        }
         return ans;
-    }   
+
+    }
 }
