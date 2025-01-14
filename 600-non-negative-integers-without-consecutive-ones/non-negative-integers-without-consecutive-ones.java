069bb199af1d80fc3cbf3f04e2fac50b3ab58827
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-01-14 18:01:33 +0530

    Time: 1 ms (85.79%) | Memory: 40.8 MB (74.74%) - LeetSync

--- 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
@@ -13,8 +13,6 @@ class Solution {
         if(n+1 == (next2spower+1)/2 )
             return indx > 1 ? arr[indx-2] : 0;
 
-        System.out.println(indx+" "+next2spower+" "+(next2spower+1)+" "+Arrays.toString(arr));
-
         int ans = Math.max(0,n + 1 -  (next2spower+1)/2 - (next2spower+1)/4)
                              + (indx > 1 ? arr[indx-2] : 0) 
                              + calculate(arr,Math.min(n-(next2spower+1)/2, (next2spower+1)/4));
@@ -28,17 +26,6 @@ class Solution {
         for(int i=1;i<32;i++){
             arr[i] = (int)Math.pow(2,i-1) +( i>1 ? arr[i-2] : 0)+ (i > 1 ? arr[i-1] : 0);
         }
-
-        int next2spower = n;
-        next2spower |= next2spower >>> 1;
-        next2spower |= next2spower >>> 2;
-        next2spower |= next2spower >>> 4;
-        next2spower |= next2spower >>> 8;
-        next2spower |= next2spower >>> 16;
-
-        int indx = (int)(Math.log(next2spower+1)/Math.log(2));
-        System.out.println(indx+" "+next2spower+" "+(next2spower+1)+" "+Arrays.toString(arr));
-        int ans = calculate(arr,n);
-        return n+1-ans;
+        return n+1- calculate(arr,n);
     }
 }
