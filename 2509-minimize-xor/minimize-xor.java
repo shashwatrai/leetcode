de1d5805fd4ed2bc21da874ad192514bff2b1646
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-01-15 09:24:19 +0530

    Time: 20 ms (3.77%) | Memory: 41.9 MB (5.66%) - LeetSync

--- 2509-minimize-xor/minimize-xor.java
+++ 2509-minimize-xor/minimize-xor.java
@@ -1,34 +1,32 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
+        
         int n = Integer.bitCount(num2);
 
+        int len = Integer.toBinaryString(num1).length();
+
+        int res =0;
         
-        char []arr = new char[32];
-        Arrays.fill(arr,'0');
-        char num1BS[] = Integer.toBinaryString(num1).toCharArray();
-        for(int i =0;i < num1BS.length;i++){
-            arr[31-i] = num1BS[num1BS.length - i - 1];
-        }
-        // System.out.println(n+" "+ Arrays.toString(num1BS) +" "+Arrays.toString(arr));
-        int indx=32 - Math.max(num1BS.length,Integer.toBinaryString(num2).length());
-        for(;indx<arr.length && n>0;indx++){
-            if(arr[indx] == '1')
+        while(len > 0 && n > 0){
+            int isSet = num1 & (1 << (len-1));
+            System.out.println(isSet);
+            if(isSet != 0){
+                res |= (1 << (len-1));
                 n--;
-        }
-        // System.out.println(indx+" "+ Arrays.toString(num1BS) +" "+Arrays.toString(arr));
-        if(n == 0){
-            while(indx < arr.length){
-                arr[indx++] ='0';
             }
-        }else{
-            for(indx = arr.length-1;indx > 0 && n > 0;indx--){
-                if(arr[indx] == '0'){
-                    arr[indx] = '1';
+            len--;
+        }
+        System.out.println(n +" "+len+" "+res);
+        if(n > 0){
+            while(n > 0){
+                int isSet = res & (1 << (len));
+                if(isSet == 0){
+                    res |= (1 << (len));
                     n--;
                 }
+                len++;
             }
         }
-        // System.out.println(n+" "+ Arrays.toString(num1BS) +" "+Arrays.toString(arr));
-        return Integer.parseInt(new String(arr),2);
+        return res;
     }
 }
