a2c55301011461bd32fb662a24e89b8e91693822
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-01-18 18:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (17.23%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count =0;
+        boolean allowed = true;
+        int prev = -1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i] > nums[i+1]){
+                if(prev > nums[i+1]){
+                    if(!allowed)
+                        return false;
+                    allowed=false;
+                    nums[i+1] = nums[i];
+                    prev = nums[i];
+                }else {
+                    if(!allowed)
+                        return false;
+                    allowed = false;
+                    prev = nums[i+1];
+                }
+            }else{
+                prev = nums[i];
+            }
+        }
+        return count <= 1;
+    }
+}
