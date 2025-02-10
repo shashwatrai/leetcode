88b1b910f21f2af576dd89f4639dfc9da62499ab
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-02-10 11:34:33 +0530

    Time: 129 ms (5.27%) | Memory: 126.9 MB (13.93%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -0,0 +1,32 @@
+class NumberContainers {
+    HashMap<Integer,Integer> indxToNo;
+    TreeMap<Integer,TreeSet<Integer>> noToIndx;
+    public NumberContainers() {
+        indxToNo = new HashMap<>();
+        noToIndx = new TreeMap<>();
+    }
+    
+    public void change(int index, int number) {
+        if(indxToNo.containsKey(index)){
+            int target = indxToNo.get(index);
+            noToIndx.get(target).remove(index); 
+        }
+        if(!noToIndx.containsKey(number))
+            noToIndx.put(number,new TreeSet<>());   
+        indxToNo.put(index,number);
+        noToIndx.get(number).add(index);
+    }
+    
+    public int find(int number) {
+        if(!noToIndx.containsKey(number) ||noToIndx.get(number).isEmpty())
+            return -1;
+        return noToIndx.get(number).first();
+    }
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
