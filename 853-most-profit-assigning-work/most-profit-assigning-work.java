8998116634fec5cc9e2be8b3ed8c807244a5d704
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-05-05 20:38:32 +0530

    Time: 17 ms (84.90%) | Memory: 45.6 MB (76.40%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -9,26 +9,16 @@ class Solution {
         Arrays.sort(arr, (a,b)-> difficulty[a] != difficulty[b] 
                                     ? difficulty[a]-difficulty[b]:a-b);
 
-        int maxProfit[] = new int[n];
-
-        // int max = 0;
-        // for(int i=0;i<n;i++){
-        //     max = Math.max(max,profit[arr[i]]);
-        //     maxProfit[i] = max;
-        // }
-        // System.out.println(Arrays.toString(arr)+" \n"+Arrays.toString(maxProfit));
         Arrays.sort(worker);
         int res=0;
         int max =0 ;
         int indx =0;
-        // Arrays.sort(difficulty);
-        // System.out.println(Arrays.toString(difficulty)+" \n"+Arrays.toString(arr)+"\n"+Arrays.toString(maxProfit));
+
         for(int i=0;i<worker.length;i++){
             while(indx < arr.length && worker[i] >= difficulty[arr[indx]]){
                 max = Math.max(max,profit[arr[indx]]);
                 indx++;
             }
-            System.out.println(worker[i]+" "+indx+" "+max);
             res += max;
         }
         return res;
