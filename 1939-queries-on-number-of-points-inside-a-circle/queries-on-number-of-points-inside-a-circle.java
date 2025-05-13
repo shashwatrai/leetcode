49e398fdc279522a7a156c7c27b484e3df6f462a
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-05-13 10:07:33 +0530

    Time: 47 ms (6.35%) | Memory: 45.6 MB (23.48%) - LeetSync

--- 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.java
+++ 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.java
@@ -1,85 +1,105 @@
+class Node{
+    int []point;
+    int op;
+    Node left, right;
+}
 class Solution {
-    public int findFloor(List<Integer> arr,int t){
-        int l = 0;
-        int r = arr.size()-1;
-
-        while(l<=r){
-            int m = (l+r)/2;
-
-            if(arr.get(m) <= t)
-                l = m+1;
-            else
-                r = m-1;
+    public void traverse(Node root, int []query,int ans[]){
+        if(root == null)
+            return;
+        
+        boolean isValid = (root.point[0] - query[0])* (root.point[0] - query[0]) + 
+             (root.point[1] - query[1]) * (root.point[1] - query[1])  <= query[2] * query[2];
+            
+        if(isValid)
+            ans[0]++;
+        // System.out.println(Arrays.toString(root.point)+" "+Arrays.toString(query));
+        if(root.op == 1){
+            if(root.point[1] <= query[1] + query[2] && root.point[1] >= query[1] - query[2]){
+                traverse(root.left,query,ans);
+                traverse(root.right,query,ans);
+            }else if(root.point[1] > query[1] + query[2]){
+                traverse(root.left,query,ans);
+            }else if(root.point[1] < query[1] - query[2]){
+                traverse(root.right,query,ans);
+            }
+        }else{
+            if(root.point[0] <= query[0] + query[2] && root.point[0] >= query[0] - query[2]){
+                traverse(root.left,query,ans);
+                traverse(root.right,query,ans);
+            }else if(root.point[0] > query[0] + query[2]){
+                traverse(root.left,query,ans);
+            }else if(root.point[0] < query[0] - query[2]){
+                traverse(root.right,query,ans);
+            }
         }
-        return r;
     }
-    public int findCeil(List<Integer> arr, int t){
-        int l = 0;
-        int r = arr.size()-1;
-
-        while(l<=r){
-            int m = (l+r)/2;
-
-            if(arr.get(m) >= t)
-                r = m-1;
-            else
-                l = m+1;
-        }
-        return l == arr.size() ? -1:l;
+    public void test(Node root){
+        if(root == null)
+            return;
+        
+        test(root.left);
+        System.out.println(Arrays.toString(root.point));
+        test(root.right);
     }
     public int[] countPoints(int[][] points, int[][] queries) {
+        // Arrays.sort(points,(a,b) -> a[0] - b[0]);
+        Node root = null;
+        for(int i=0;i<points.length;i++){
+            Node curr = new Node();
+            curr.point  = points[i];
+            if(root == null){
+                curr.op = 0;
+                root = curr;
+            }else{
+                Node temp = root,prev = null;
+                System.out.println();
+                while(temp != null){
+                    prev = temp;
+                    
+                    if(temp.op == 0){
+                        if(temp.point[0] > curr.point[0]){
+                            temp = temp.left;
+                        }else{
+                            // System.out.println("right "+Arrays.toString(temp.point));
+                            temp = temp.right;
+                        }
 
-        TreeMap<Integer,List<Integer>> tmap = new TreeMap<>();
-
-        for(int p[]:points){
-            if(!tmap.containsKey(p[0])){
-                tmap.put(p[0],new ArrayList<>());
-            }
-            tmap.get(p[0]).add(p[1]);
-        }
-        for(int i:tmap.keySet()){
-            Collections.sort(tmap.get(i));
-        }
-        int n = queries.length;
-
-        int ans[] = new int[n];
-
-        for(int i=0;i<n;i++){
-            int lx  = queries[i][0] - queries[i][2];
-            int ux = queries[i][0] + queries[i][2];
-
-            int ly = queries[i][1] - queries[i][2];
-            int uy = queries[i][1] + queries[i][2];
-
-            Integer start = tmap.ceilingKey(lx);
-
-            if(start == null){
-                ans[i] = 0;
-                continue;
-            }
-            Integer end = tmap.floorKey(ux);
-            
-            if(end == null){
-                ans[i] = 0;
-                continue;
-            }
-            
-            while(start != null && start <= end){
-                List<Integer> arr = tmap.get(start);
-                Integer starty = findCeil(arr,ly);
-                Integer endy = findFloor(arr,uy);
-                // System.out.println(i+" "+start+" "+end+" "+starty+" "+endy);
-                if(starty != -1 && endy != -1){
-                    while(starty <= endy){
-                        boolean flag = (queries[i][0]- start) * (queries[i][0]- start) +
-                        (queries[i][1]- arr.get(starty)) * (queries[i][1]- arr.get(starty)) <= queries[i][2] * queries[i][2];
-                        if(flag)
-                            ans[i]++;
-                        starty++;
+                    }else{
+                        if(temp.point[1] > curr.point[1]){
+                            temp = temp.left;
+                        }else{
+                            // System.out.println("right "+Arrays.toString(temp.point));
+                            temp = temp.right;
+                        }
                     }
                 }
-                start = tmap.ceilingKey(start+1);
-            }
+                // System.out.println(Arrays.toString(prev.point)+" "+prev.op);
+                if(prev.op == 0){
+                    curr.op = 1;
+                    if(prev.point[0] > curr.point[0]){
+                        prev.left = curr;
+                    }else
+                        prev.right = curr;
+                }else{
+                    curr.op = 0;
+                    if(prev.point[1] > curr.point[1]){
+                        prev.left = curr;
+                    }else{
+                        // System.out.println("curr" + Arrays.toString(curr.point));
+                        prev.right = curr;
+                    }
+                }
+            }   
+        }
+
+        // test(root);
+        int ans[] = new int[queries.length];
+
+        for(int i=0;i<ans.length;i++){
+            int res[] = new int[1];
+            traverse(root,queries[i],res);
+            ans[i] = res[0];
         }
         return ans;
     }
