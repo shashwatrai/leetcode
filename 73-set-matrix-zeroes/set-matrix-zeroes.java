d7895433efe2214b4c62eb41388e7cfa81de1a42
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-05-21 08:12:34 +0530

    Time: 1 ms (72.83%) | Memory: 45.4 MB (96.99%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,48 +1,60 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean row = false;
-        boolean column = false;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0){
-                    if(i == 0&&j ==0 ){
-                        row = column = true;
-                    }else if(i == 0)
-                        row = true;
-                    else if(j == 0)
-                        column = true;
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                }
-            }
-        }
-        for(int i=1;i<m;i++){
-            if(matrix[i][0] == 0){
-                for(int j=1;j<n;j++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for(int j=1;j<n;j++){
-            if(matrix[0][j] == 0){
-                for(int i=1;i<m;i++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+        int flag =0;
+    
         if(matrix[0][0] == 0){
-            if(row){
-                for(int i=0;i<n;i++){
-                    matrix[0][i] = 0;
+            flag = 3;
+        }
+
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j =0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    if(i == 0 && j!= 0){
+                        if(flag == 2)
+                            flag = 3;
+                        else if(flag == 0)
+                            flag = 1;
+                    }
+                    else if(i != 0 && j == 0){
+                        if(flag == 1)
+                            flag = 3;
+                        else if(flag == 0)
+                            flag = 2;
+                    }
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
-            if(column){
-                for(int i=0;i<m;i++)
-                    matrix[i][0] =0;
+        }
+
+        for(int i=1;i<matrix.length;i++){
+            if(matrix[i][0] == 0){
+                for(int j =1 ;j < matrix[i].length;j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j=1;j<matrix[0].length;j++){
+            if(matrix[0][j] == 0){
+                for(int i=1;i<matrix.length;i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(flag == 3){
+            for(int i=0;i<matrix.length;i++)
+                matrix[i][0] = 0;
+            for(int i=0;i<matrix[0].length;i++)
+                matrix[0][i] = 0;
+        }else if(flag == 2){
+            for(int i=0;i<matrix.length;i++)
+                matrix[i][0] = 0;
+        }else if(flag == 1){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i] = 0;
             }
         }
     }
-
 }
