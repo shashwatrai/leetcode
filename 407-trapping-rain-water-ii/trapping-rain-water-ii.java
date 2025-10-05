5fc37631e3f7b1bbb2f792c5e68b1f1190b25697
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-10-05 10:10:46 +0530

    Time: 32 ms (6.04%) | Memory: 45.8 MB (96.83%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -1,41 +1,66 @@
 class Solution {
     public int trapRainWater(int[][] heightMap) {
-        
         int m = heightMap.length;
         int n = heightMap[0].length;
-        boolean vis[][] = new boolean[m][n];
-        boolean boundary[][] = new boolean[m][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> heightMap[a[0]][a[1]] - heightMap[b[0]][b[1]]);
-        for(int i=0;i<m;i++){
-            pq.add(new int[]{i,0});
-            pq.add(new int[]{i,n-1});
-            boundary[i][0] = boundary[i][n-1] =true;
-        }
-        for(int j=0;j<n;j++){
-            pq.add(new int[]{0,j});
-            pq.add(new int[]{m-1,j});
-            boundary[0][j] = boundary[m-1][j] =true;
-        }
-        int ans=0;
-       
-        int dir[][] = {{0,-1},{0,1},{-1,0},{1,0}};
-        while(!pq.isEmpty()){
-            int []curr= pq.poll();
-            vis[curr[0]][curr[1]] = true;
-            for(int i=0;i<4;i++){
-                int x = curr[0] + dir[i][0];
-                int y = curr[1] + dir[i][1];
 
-                if(x < 0 || y < 0 || x >= m || y >= n || vis[x][y] || boundary[x][y])
-                    continue;
-                
-                
-                
-                ans += Math.max(0, heightMap[curr[0]][curr[1]]-heightMap[x][y]);
-                heightMap[x][y] = Math.max(heightMap[x][y],heightMap[curr[0]][curr[1]]);
-                pq.add(new int[]{x,y});
-                boundary[x][y] = true;
-                
+        int [][]boundary = new int[m][n];
+
+        for(int i[]:boundary){
+            Arrays.fill(i,-1);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> boundary[a[0]][a[1]] - boundary[b[0]][b[1]]);
+
+        for(int j = 0;j<n;j++){
+            boundary[0][j] = heightMap[0][j];
+            pq.add(new int[]{0,j});
+
+            boundary[m-1][j] = heightMap[m-1][j];
+            pq.add(new int[]{m-1,j});
+
+        }
+
+        for(int i = 0;i<m;i++){
+            boundary[i][0] = heightMap[i][0];
+            pq.add(new int[]{i,0});
+
+            boundary[i][n-1] = heightMap[i][n-1];
+            pq.add(new int[]{i,n-1});
+        }
+
+        int ans =0 ;
+        while(!pq.isEmpty()){
+            int curr[] = pq.poll();
+
+            int r = curr[0];
+            int c = curr[1];
+
+            // System.out.println(r+" "+c);
+
+            if(r > 0 && boundary[r-1][c] == -1){
+                ans += Math.max(boundary[r][c] - heightMap[r-1][c],0);
+                boundary[r-1][c] = Math.max(boundary[r][c],heightMap[r-1][c]);
+                pq.add(new int[]{r-1,c});
+                // System.out.println("("+(r-1)+","+c+")"+ " "+ans);
+            }
+            if(c > 0 && boundary[r][c-1] == -1){
+                ans += Math.max(boundary[r][c] - heightMap[r][c-1],0);
+                boundary[r][c-1] = Math.max(boundary[r][c],heightMap[r][c-1]);
+                pq.add(new int[]{r,c-1});
+                // System.out.println("("+(r)+","+(c-1)+")"+ " "+ans);
+            }
+            if(r < m-1 && boundary[r+1][c] == -1){
+                ans += Math.max(boundary[r][c] - heightMap[r+1][c],0);
+                boundary[r+1][c] = Math.max(boundary[r][c],heightMap[r+1][c]);
+                pq.add(new int[]{r+1,c});
+                // System.out.println("("+(r+1)+","+c+")" + " "+ans);
+
+            }
+            if(c < n-1 && boundary[r][c+1] == -1){
+                ans += Math.max(boundary[r][c] - heightMap[r][c+1],0);
+                boundary[r][c+1] = Math.max(boundary[r][c],heightMap[r][c+1]);
+                pq.add(new int[]{r,c+1});
+                // System.out.println("("+(r)+","+(c+1)+")"+ " "+ans);
             }
         }
         return ans;
