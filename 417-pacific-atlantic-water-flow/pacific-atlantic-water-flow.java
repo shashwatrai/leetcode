056d14ea62fdc241bfb0a76704e3f8db3b5686e5
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-10-05 09:24:39 +0530

    Time: 12 ms (24.76%) | Memory: 45.5 MB (74.79%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,85 +1,68 @@
 class Solution {
-    public void traversePacific(int [][]heights,boolean [][]status){
-        Queue<int []> queue  = new LinkedList<>();
 
-        for(int i=0;i<heights.length;i++){
-            status[i][0] = true;
-            queue.add(new int[]{i,0});
-        }
-        for(int i=0;i<heights[0].length;i++){
-            status[0][i] =true;
-            queue.add(new int[]{0,i});
-        }
+    public void traverse(int [][]heights,int [][]status, List<List<Integer>> start){
 
-        while(!queue.isEmpty()){
-            int []pos = queue.poll();
-            int r = pos[0],c = pos[1];
-            if( r > 0 && !status[r-1][c] && heights[r-1][c] >= heights[r][c]){
-                status[r-1][c] = true;
-                queue.add(new int[]{r-1,c});
-            }
-            if( c > 0 && !status[r][c-1] && heights[r][c-1] >= heights[r][c]){
-                status[r][c-1] = true;
-                queue.add(new int[]{r,c-1});
-            }
-            if( r < heights.length-1 && !status[r+1][c] && heights[r+1][c] >= heights[r][c]){
-                status[r+1][c] = true;
-                queue.add(new int[]{r+1,c});
-            }
-            if( c < heights[0].length -1  && !status[r][c+1] && heights[r][c+1] >= heights[r][c]){
-                status[r][c+1] = true;
-                queue.add(new int[]{r,c+1});
-            }
-        }
-    }
-    public void traverseAntalntic(int [][]heights,boolean [][]status){
-        Queue<int []> queue  = new LinkedList<>();
+        Queue<List<Integer>> que = new LinkedList<>(start);
+        int dir[][] = {{-1,0},{1,0},{0,1},{0,-1}};
+        while(!que.isEmpty()){
+            List<Integer> curr = que.poll();
 
-        for(int i=0;i<heights.length;i++){
-            status[i][heights[0].length-1] = true;
-            queue.add(new int[]{i,heights[0].length-1});
-        }
-        for(int i=0;i<heights[0].length;i++){
-            status[heights.length-1][i] =true;
-            queue.add(new int[]{heights.length-1,i});
+            int r = curr.get(0);
+            int c = curr.get(1);
+
+            for(int i=0;i<4;i++){
+                int x = r + dir[i][0];
+                int y = c + dir[i][1];
+
+                if(x < 0 || y < 0 || x >= heights.length || y >= heights[0].length || status[x][y] != 0 || heights[r][c] > heights[x][y]){
+                    continue;
+                }
+                status[x][y] = 1;
+                que.add(Arrays.asList(x,y));
+            }
         }
 
-        while(!queue.isEmpty()){
-            int []pos = queue.poll();
-            int r = pos[0],c = pos[1];
-            if( r > 0 && !status[r-1][c] && heights[r-1][c] >= heights[r][c]){
-                status[r-1][c] = true;
-                queue.add(new int[]{r-1,c});
-            }
-            if( c > 0 && !status[r][c-1] && heights[r][c-1] >= heights[r][c]){
-                status[r][c-1] = true;
-                queue.add(new int[]{r,c-1});
-            }
-            if( r < heights.length-1 && !status[r+1][c] && heights[r+1][c] >= heights[r][c]){
-                status[r+1][c] = true;
-                queue.add(new int[]{r+1,c});
-            }
-            if( c < heights[0].length -1  && !status[r][c+1] && heights[r][c+1] >= heights[r][c]){
-                status[r][c+1] = true;
-                queue.add(new int[]{r,c+1});
-            }
-        }
     }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int m = heights.length;
-        int n = heights[0].length;  
+        int n = heights[0].length;
 
-        boolean status1[][] = new boolean[m][n] ;
-        boolean status2[][] = new boolean[m][n];
-        traversePacific(heights,status1);
-        traverseAntalntic(heights,status2);
-        List<List<Integer>>res = new ArrayList<>();
+        
+        List<List<Integer>> source1 = new ArrayList<>();
+        List<List<Integer>> source2 = new ArrayList<>();
+        int [][]status1 = new int[m][n];
+        int [][]status2 = new int[m][n]; 
+        for(int i=0;i<n;i++){
+            source1.add(Arrays.asList(0,i));
+            source2.add(Arrays.asList(m-1,i));
+            status1[0][i] = status2[m-1][i] = 1;
+        }
+
+        for(int i=0;i<m;i++){
+            source1.add(Arrays.asList(i,0));
+            source2.add(Arrays.asList(i,n-1));
+            status1[i][0] = status2[i][n-1] = 1;
+        }   
+
+        
+        
+        traverse(heights,status1,source1);
+        traverse(heights,status2,source2);
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(status1[i][j] && status2[i][j])
-                    res.add(Arrays.asList(i,j));
+                status1[i][j] += status2[i][j];
             }
         }
-        return res;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(status1[i][j] == 2)
+                    ans.add(Arrays.asList(i,j));
+            }
+        }
+        return ans;
     }
 }
