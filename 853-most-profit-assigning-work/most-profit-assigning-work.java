b58b87ed04dea379e9faeb43ff5f8f9af3758dab
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-10-08 08:46:40 +0530

    Time: 12 ms (87.36%) | Memory: 46.4 MB (10.18%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,25 +1,19 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int  n =difficulty.length;
+        int ans[] = new int[100001];
 
-        Integer arr[] = new Integer[n];
-        for(int i=0;i<n;i++)
-            arr[i] = i;
+        for(int i=0;i<difficulty.length;i++){
+            ans[difficulty[i]] = Math.max(ans[difficulty[i]],profit[i]);
+        }
 
-        Arrays.sort(arr, (a,b)-> difficulty[a] != difficulty[b] 
-                                    ? difficulty[a]-difficulty[b]:a-b);
-
-        Arrays.sort(worker);
-        int res=0;
-        int max =0 ;
-        int indx =0;
-
-        for(int i=0;i<worker.length;i++){
-            while(indx < arr.length && worker[i] >= difficulty[arr[indx]]){
-                max = Math.max(max,profit[arr[indx]]);
-                indx++;
-            }
-            res += max;
+        int max = 0;
+        for(int i=0;i<ans.length;i++){
+            ans[i] = Math.max(ans[i],max);
+            max = Math.max(ans[i],max);
+        }
+        int res =0;
+        for(int i: worker){
+            res += ans[i];
         }
         return res;
     }
