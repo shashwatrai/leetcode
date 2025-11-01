122f76103160a035d1a78df0577803de038825c3
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2025-11-01 23:17:09 +0530

    Time: 23 ms (18.48%) | Memory: 178.2 MB (5.52%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -11,21 +11,22 @@
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
         Set<Integer> set = new HashSet<>();
+
         for(int i:nums){
             set.add(i);
         }
-        ListNode pHead = new ListNode(-1,head);
 
-        ListNode prev = pHead;
+        ListNode newHead = new ListNode(-1);
+        ListNode curr = newHead;
         while(head != null){
-            if(set.contains(head.val)){
-                prev.next = head.next;
-                
-            }else{
-                prev = head;
+            if(!set.contains(head.val)){
+                curr.next = head;
+                curr = head;
             }
             head = head.next;
+            
         }
-        return pHead.next;
+        curr.next = null;
+        return newHead.next;
     }
 }
