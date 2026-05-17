89b75a8d7a19857209371450a8936bd5a0710847
shashwatrai <46484010+shashwatrai@users.noreply.github.com>
2026-05-17 19:26:39 +0530

    Time: 1 ms (99.98%) | Memory: 42.9 MB (29.99%) - LeetSync

--- 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.java
+++ 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int calculate(double b, double r, boolean ceil){
+    public int calculate(double b, double r){
         double res = 1f;
         double p = 1f;
         while(r > 0f){
@@ -10,21 +10,16 @@ class Solution {
             p = p/2f;
             b = Math.sqrt(b);
         }
-        return ceil ? (int)Math.ceil(res): (int)Math.floor(res);
+        return  (int)Math.ceil(res);
     }
     public int countKthRoots(int l, int r, int k) {
         if(k == 1)
             return r - l + 1;
-        int ul = calculate((double) r , (double)1/k, true);
-        int ll = calculate((double) l , (double)1/k , false);
-        // System.out.println(ul+" "+ll+" "+r+" "+l +" "+k);
-        int ans = 0;
+        int ul = calculate((double) r , (double)1/k);
+        int ll = calculate((double) l , (double)1/k);
         
-        for(int i =ll;i<=ul;i++){
-            int temp = (int)Math.pow(i,k) ;
-            if(temp <= r && temp >= l)
-                ans++;
-        }
-        return ans;
+        boolean boundary = Math.pow(ul,k) <= r;
+
+        return ul - ll + (boundary ? 1: 0); 
     }
 }
